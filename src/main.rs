18588648991e86befac09d542dc78301ef9c2a14
Bene <bene.eble.work@gmail.com>
2020-10-03 13:55:56 +0200

    added handler to show all days with images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use chrono::Utc;
 use log::*;
-use std::{fs, path::PathBuf, thread, time};
+use serde_json::json;
+use std::path::Path;
+use std::{fs, io, path::PathBuf, thread, time};
 
 use actix_web::{get, http::StatusCode, middleware, App, HttpResponse, HttpServer};
 use clap::Clap;
@@ -24,6 +26,15 @@ async fn api() -> HttpResponse {
         .body("{}")
 }
 
+
+#[get("/api/days")]
+async fn days_with_photos() -> HttpResponse {
+    let response = json!(dirs_with_images(&Opts::parse().image_dir).unwrap());
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/json; charset=utf-8")
+        .body(response)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     if std::env::var("RUST_LOG").is_err() {
@@ -34,7 +45,10 @@ async fn main() -> std::io::Result<()> {
     info!("Starting camera loop");
     thread::spawn(move || loop {
         let image_name = format!("{}.jpg", Utc::now().to_rfc3339());
-        let image_dir = Opts::parse().image_dir.clone().join(&format!("{}", Utc::today()));
+        let image_dir = Opts::parse()
+            .image_dir
+            .clone()
+            .join(&format!("{}", Utc::today()));
         fs::create_dir_all(&image_dir).unwrap();
         let status = std::process::Command::new("raspistill")
             .args(&[
@@ -71,9 +85,24 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(actix_files::Files::new("/images", &image_dir).show_files_listing())
             .service(api)
+            .service(days_with_photos)
     })
     .bind(opts.address)?
     .workers(1)
     .run()
     .await
+}
+
+fn dirs_with_images(image_dir: &Path) -> io::Result<Vec<String>> {
+    let mut dirs = Vec::new();
+    if image_dir.is_dir() {
+        for entry in fs::read_dir(image_dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() && fs::read_dir(path)?.count() > 0 {
+                dirs.push(entry.file_name().into_string().unwrap());
+            }
+        }
+    };
+    Ok(dirs)
 }
